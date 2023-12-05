0113b5a18bc66e9361ab2a7bcf48f86ce5ea938e
Techie <149437995+FarRza111@users.noreply.github.com>
2023-12-05 12:49:28 -0800

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,52 +1,55 @@
-
 import json
 from typing import Final
 import requests
 
-
 class GetData:
-    def __init__(self, url, api):
+    def __init__(self, url, api, currency):
         self.url = url
-        self.api = api
+        self._api =  '924b8e007b6804e9d6c74acfb501e218'
+        self.currency = currency
+
+    @property
+    def api(self):
+        return self._api
+    @api.setter
+    def api(self, value):
+        # You can add additional validation logic here if needed
+        self._api = value
 
     def fetch_data(self):
         pay_amnt: dict = {'access_key': self.api}
-        request = requests.get(url= self.url, params=pay_amnt)
-
+        request = requests.get(url=self.url, params=pay_amnt)
         try:
-            if request.status_code == 200:
-                data  = requests.get(url=BASE_URL, params= pay_amnt)
-                return data.json()
+            request.status_code == 200
+            data = requests.get(url=self.url, params=pay_amnt)
+            return data.json()
         except requests.exceptions.RequestException as e:
-            print(f"This is error that occured {e}")
+            print(f"This is the error that occurred: {e}")
+            return None
 
-        return None
+    def get_currencies(self):
+        returned_data = self.fetch_data()
+        data_amount = returned_data.get('rates', None)
+        base_currency = returned_data.get('base')
+        base_currency_amnt = data_amount.get(base_currency)
+        foreign_currency_amnt = data_amount.get(self.currency)
+        return base_currency_amnt, foreign_currency_amnt, base_currency, self.currency
 
-    def get_currency(self, currency_check):
-        currency_check: str = currency_check.upper()
-
-        data_currency = self.fetch_data().get('rates',None)
-        if currency_check in data_currency.keys():
-            return data_currency[currency_check]
-        else:
-            raise ValueError(f'{currency_check} does not appear in the currency keys')
-
-
-# class Currency:
-#     pass
-#
-# class GetCurrency:
-#     pass
-#
-# class ConvertCurrency:
-#     pass
-
+    @property
+    def conversion_rate(self):
+        ba, fv, base_currency, foreign_currency = self.get_currencies()
+        conversion = ba / fv
+        print(f"{base_currency} base amount to {foreign_currency} amount is {conversion:.2f}")
+        return conversion
 
 if __name__ == "__main__":
-
     BASE_URL: Final[str] = 'http://api.exchangeratesapi.io/v1/latest'
-    API_KEY: Final[str] = '924b8e007b6804e9d6c74acfb501e218'
-    cs = GetData(BASE_URL, API_KEY)
-    # print(cs.fetch_data())
-    print(cs.get_currency('AWG'))
+    # API_KEY: Final[str] = '924b8e007b6804e9d6c74acfb501e218'
+    cs = GetData(BASE_URL, GetData.api,'AUD')
+    conversion_result = cs.conversion_rate
+
+    if conversion_result is not None:
+        print(f'conversion result is {conversion_result}')
+    else:
+        print(f'something is wrong with conversion rate')
 
